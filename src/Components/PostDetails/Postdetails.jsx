3c37ae500186595f29d1d938f9b152c469eeec3f
vatiza <rakibur.mac@gmail.com>
2024-03-03 15:02:49 +0600

    back to previews page

--- src/Components/PostDetails/Postdetails.jsx
+++ src/Components/PostDetails/Postdetails.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const Postdetails = () => {
     const post = useLoaderData();
     const { id, title, userId, body } = post;
+    const navigate = useNavigate();
+    const hanelGoBack = () => {
+        navigate(-1);
+    }
     return (
         <div>
             <h1>Details about this post</h1>
             <h2>user Id:{id}</h2>
             <h4>Title:{title}</h4>
             <p><small>{body}</small></p>
-           
+            <button onClick={hanelGoBack}>Go Back</button>
+
 
         </div>
     );
